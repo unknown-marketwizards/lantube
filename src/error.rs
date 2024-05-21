57286ead05369f3858acbec28156575b30e3191f
marketwizards <marketwizard26@gmail.com>
2024-05-21 16:19:05 +0800

    error handling

---
+++ src/error.rs
@@ -0,0 +1,26 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+
+pub struct AppError(anyhow::Error);
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use axum::http::StatusCode;
+mod error;
+
+use crate::error::AppError;
+
+use axum::extract::{DefaultBodyLimit, State};
 use axum::{extract::Multipart, routing::post, Json, Router};
 use clap::Parser;
-use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 use tokio::fs;
 use tower_http::{
@@ -9,11 +12,9 @@ use tower_http::{
     trace::TraceLayer,
 };
 
-use axum::extract::DefaultBodyLimit;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::str::FromStr;
-use std::sync::RwLock;
 
 #[derive(Parser, Debug)]
 #[command(version, long_about = None)]
@@ -24,8 +25,9 @@ struct Args {
     addr: Option<String>,
 }
 
-lazy_static! {
-    static ref DATA_DIR: RwLock<PathBuf> = RwLock::new(PathBuf::new());
+#[derive(Default, Clone)]
+struct AppState {
+    data_dir: PathBuf,
 }
 
 #[tokio::main]
@@ -38,7 +40,9 @@ async fn main() {
         SocketAddr::from(([127, 0, 0, 1], 8888))
     };
 
-    *DATA_DIR.write().unwrap() = PathBuf::from(args.data_dir.clone());
+    let state = AppState {
+        data_dir: PathBuf::from(&args.data_dir),
+    };
 
     let app = Router::new()
         .nest_service("/", ServeFile::new("./frontend/dist/index.html"))
@@ -47,7 +51,8 @@ async fn main() {
         .route("/api/explorer", post(explorer))
         .route("/api/mkdir", post(mkdir))
         .route("/api/upload", post(upload))
-        .layer(DefaultBodyLimit::disable());
+        .layer(DefaultBodyLimit::disable())
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
@@ -68,8 +73,8 @@ struct PathParams {
     path: String,
 }
 
-fn get_path(path: String) -> PathBuf {
-    DATA_DIR.read().unwrap().join(if path.starts_with("/") {
+fn get_path(data_dir: &PathBuf, path: String) -> PathBuf {
+    data_dir.join(if path.starts_with("/") {
         &path[1..]
     } else {
         path.as_str()
@@ -77,25 +82,32 @@ fn get_path(path: String) -> PathBuf {
 }
 
 async fn explorer(
+    State(state): State<AppState>,
     Json(PathParams { path }): Json<PathParams>,
-) -> (StatusCode, Json<Vec<ExplorerItem>>) {
-    let dir = get_path(path);
+) -> Result<Json<Vec<ExplorerItem>>, AppError> {
+    let dir = get_path(&state.data_dir, path);
 
     let mut result = Vec::new();
 
-    let mut entries = fs::read_dir(dir).await.unwrap();
-    while let Some(entry) = entries.next_entry().await.unwrap() {
+    let mut entries = fs::read_dir(dir).await?;
+    while let Some(entry) = entries.next_entry().await? {
         let path = entry.path();
+        let file_name = if let Some(f) = path.file_name() {
+            f.to_string_lossy().into_owned()
+        } else {
+            continue;
+        };
+
         if path.is_dir() {
             result.push(ExplorerItem {
-                name: path.file_name().unwrap().to_string_lossy().into_owned(),
+                name: file_name,
                 type_: "dir".to_string(),
             });
         } else {
             if let Some(ext) = path.extension() {
                 if is_supported_file_type(ext) {
                     result.push(ExplorerItem {
-                        name: path.file_name().unwrap().to_string_lossy().into_owned(),
+                        name: file_name,
                         type_: "file".to_string(),
                     });
                 }
@@ -103,41 +115,48 @@ async fn explorer(
         }
     }
 
-    (StatusCode::OK, Json(result))
+    Ok(Json(result))
 }
 
-async fn mkdir(Json(PathParams { path }): Json<PathParams>) -> StatusCode {
-    let dir = get_path(path);
+async fn mkdir(
+    State(state): State<AppState>,
+    Json(PathParams { path }): Json<PathParams>,
+) -> Result<(), AppError> {
+    let dir = get_path(&state.data_dir, path);
 
-    fs::create_dir(dir).await.unwrap();
+    fs::create_dir(dir).await?;
 
-    StatusCode::OK
+    Ok(())
 }
 
-async fn upload(mut multipart: Multipart) {
+async fn upload(State(state): State<AppState>, mut multipart: Multipart) -> Result<(), AppError> {
     let mut target_path: Option<PathBuf> = None;
 
-    while let Ok(Some(field)) = multipart.next_field().await {
+    while let Some(field) = multipart.next_field().await? {
         if let Some(name) = field.name() {
             match name {
                 "file" => {
                     if let Some(dir) = &target_path {
-                        let filename = dir.join(field.file_name().unwrap());
+                        if let Some(f) = field.file_name() {
+                            let filename = dir.join(f);
 
-                        if let Ok(bytes) = field.bytes().await {
-                            fs::write(&filename, &bytes).await.unwrap();
+                            if let Ok(bytes) = field.bytes().await {
+                                fs::write(&filename, &bytes).await?;
+                            }
                         }
                     }
                 }
                 "path" => {
-                    let path_str = field.text().await.unwrap();
+                    let path_str = field.text().await?;
 
-                    target_path = Some(get_path(path_str.clone()));
+                    target_path = Some(get_path(&state.data_dir, path_str.clone()));
                 }
                 _ => {}
             }
         }
     }
+
+    Ok(())
 }
 
 fn is_supported_file_type(ext: &std::ffi::OsStr) -> bool {
